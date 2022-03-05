9f093298c35c0805f334d9e868b62692ebcd58f1
GagikBabayan <babayan-gago01@mail.ru>
2022-03-05 11:13:40 +0400

    Homework 3 (Check letter)

---
+++ cpp/day2/3_alphabet.cpp
@@ -0,0 +1,47 @@
+/*
+Write a program to input an alphabet and check whether it is vowel or consonant using switch case.
+ program to check vowel or consonant using switch case. Logic to check vowel or consonant using switch case.
+
+Example
+InputInput alphabet: c
+Output
+'c' is consonant
+
+*/
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+
+char  letter;
+
+cout << "Input letter: ";
+cin  >> letter;
+
+cout << letter;
+
+switch (letter) {
+
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u':      cout << " is vowel ";    break;
+
+
+default:       cout << " is consonant"; 
+
+}
+
+
+cout << endl;
+
+
+return(0);
+}
