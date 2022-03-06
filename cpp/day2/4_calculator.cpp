222c4f60fd72afe1eb361ae097dc739ca8d936ff
GagikBabayan <babayan-gago01@mail.ru>
2022-03-06 23:07:07 +0400

    Homework 4 (calculator)

--- cpp/day2/4_calculator.cpp
+++ cpp/day2/4_calculator.cpp
@@ -24,31 +24,46 @@ using namespace std;
 
 int main ()  {
 
-double number1,number2;
+double number1 = 0,number2 = 0;
 char operation;
 
 cout << "Input Number 1: ";
 cin  >> number1;
 
+cout << number1;
 cout << "Input  +,-,*,/ Operation ";
 cin  >>  operation;
+
 
 cout << "Input Number 2: ";
 cin  >> number2;
 
+
+cout << endl;
+
+  
+
 switch (operation) {
 
-case '+': cout << number1 << " + " << number2 << " = " << number1+number2;     break;
+case '+': cout << number1 << " + " << number2 << " = " << number1+number2;        break;
 
-case '-': cout << number1 << " - " << number2 << " = " << number1-number2;     break;
+case '-': cout << number1 << " - " << number2 << " = " << number1-number2;        break;
 
-case '*': cout << number1 << " * " << number2 << " = " << number1*number2;     break;
+case '*': cout << number1 << " * " << number2 << " = " << number1*number2;        break;
 
-case '/': cout << number1 << " / " << number2 << " = " << number1/number2;     break;
 
-default:  cout << "Error this  Operation don't find ";
+case '/': if (number2 == 0)   cout << "Error number2 = 0 ";
+
+          else cout << number1 << " / " << number2 << " = " << number1/number2;   break;
+
+
+default:  cout << "Error  this  Operation don't find ";
 
 }
+
+
+ 
+
 
 cout << endl;
 
