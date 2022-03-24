f31edcc51979bfe87afc704598487c108da15755
GagikBabayan <100302868+GagikBabayan@users.noreply.github.com>
2022-03-24 09:49:15 +0400

    Create corrected.cpp

---
+++ cpp/day12/corrected.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string correct(string word) {
+  ifstream fonc;
+  fonc.open("dictionary.txt");
+  string word_now;
+  int count = 0;
+  while(!fonc.eof()) {
+    count = 0;
+    getline(fonc, word_now);
+
+    if(word_now.size() != word.size()){
+      continue;
+    }
+    for(int i = 0; word[i] != '\0'; i++) {
+      if(word[i] != word_now[i] && word[i] + 32 != word_now[i]){
+        count ++;
+      }
+    }
+    if(count == 1) {
+      break;
+    }
+  }
+  fonc.close();
+  if(count != 1) {
+    return word;
+  }
+  return word_now;
+}
+
+int main() {
+  string line;
+  cout << "Input: ";
+  getline(cin, line);
+  line += " ";
+  string word;
+  int i = 0;
+  ofstream text;
+  text.open("corrected.txt");
+  while(line[i] != '\0') {
+    word = "";
+    for(;line[i] != ' '; i++){
+      word += line[i];
+    }
+    i++;
+    text << correct(word) << " ";
+         text << correct(word) << " ";
+  }
+  text.close();
+  return 0;
+}
+                                                                                                                                            1,1           Top
+
