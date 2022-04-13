3997d7331b925ad15076cd3c1aa695335ac87412
GagikBabayan <babayan-gago01@mail.ru>
2022-04-13 22:06:38 +0400

    Polynomial function

---
+++ oop/day2/polynomial.cpp
@@ -0,0 +1,100 @@
+#include "polynomial.h"
+#include <iostream>
+using namespace std;
+ 
+
+void Polynomial::Add() {
+  if (_degree1 > _degree2) {
+      for (int i = 0; i <= _degree1; i++) {
+          if (i <= _degree2) {
+              arr3[i] = arr1[i] + arr2[i];
+          } else {
+              arr3[i] = arr2[i];
+          }
+          cout << arr3[i]  << "x^" << i << " + ";
+      }  
+      cout << endl;  
+  } else if (_degree1 < _degree2) {
+      for (int i = 0; i <= _degree2; i++) {
+          if (i <= _degree1)  {
+              arr3[i] = arr1[i] + arr2[i];
+          } else {
+              arr3[i] = arr1[i];
+          }
+          cout << arr3[i] << "x^" << i << " + ";
+      }
+      cout << endl; 
+  } else if (_degree1 = _degree2) {
+      for (int i = 0; i <= _degree2; i++) {
+          arr3[i] = arr1[i] + arr2[i];
+          cout << arr3[i] << "x^" << i << " + ";
+      }
+      cout << endl; 
+  }
+} 
+
+void Polynomial::Subtract() {
+  if (_degree1 > _degree2) {
+      for (int i = 0; i <= _degree1; i++) {
+          if (i <= _degree2) {
+              arr3[i] = arr1[i] - arr2[i];
+          } else {
+              arr3[i] = arr2[i];
+          }
+          cout << arr3[i] << "x^" << i << " - ";
+      }   
+      cout << endl;
+  } else if (_degree1 < _degree2) {
+      for (int i = 0; i <= _degree2; i++) {
+          if (i <= _degree1)  {
+              arr3[i] = arr1[i] - arr2[i];
+          } else {
+              arr3[i] = arr1[i];
+          }
+          cout << arr3[i] << "x^" << i << " - ";
+      } 
+      cout << endl;
+  } else if (_degree1 = _degree2) {
+      for (int i = 0; i <= _degree2; i++) {
+          if (arr1[i] - arr2[i] != 0) {
+              arr3[i] = arr1[i] - arr2[i];
+          }
+          cout << arr3[i] << "x^" << i << " - ";
+      }
+      cout << endl; 
+  }
+}    
+
+void Polynomial::Multiply() {
+  if (_degree1 > _degree2) {
+      for (int i = 0; i <= _degree1; i++) {
+          if (i <= _degree2) {
+              arr3[i] = arr1[i] + arr2[i];
+          } else {
+              arr3[i] = arr2[i];
+          }
+          cout << arr3[i] << "x^" << i << " * ";
+
+      }   
+      cout << endl;  
+  } else if (_degree1 < _degree2) {
+      for (int i = 0; i <= _degree2; i++) {
+          if (i <= _degree1)  {
+              arr3[i] = arr1[i] + arr2[i];
+          } else {
+              arr3[i] = arr1[i];
+          }
+          cout << arr3[i] << "x^" << i << " * ";
+       }
+       cout << endl;  
+  } else if (_degree1 = _degree2) {
+      for (int i = 0; i <= _degree2; i++) {
+          arr3[i] = arr1[i] + arr2[i];
+          cout << arr3[i] << "x^" << i << " * ";
+      }  
+
+  }
+      cout << endl;  
+} 
+
+
