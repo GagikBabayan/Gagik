d3a2cfd44e2d5f68bd43f953afb996a02a77b7f0
GagikBabayan <babayan-gago01@mail.ru>
2022-04-13 22:52:31 +0400

    Programmer.h

---
+++ oop/day1/programmer.h
@@ -0,0 +1,8 @@
+#include "human.h";
+#include "homosapience.h"
+
+class Programmer : public Human, public Homosapience {
+    private:  string _compLevel;
+                            
+    public void HardWork();
+};
