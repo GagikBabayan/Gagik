fee28445b27fe17cdf8ce975d7890f9c4048a7b1
GagikBabayan <babayan-gago01@mail.ru>
2022-04-17 18:37:56 +0400

    Polynomial.h

--- oop/day2/polynomial.h
+++ oop/day2/polynomial.h
@@ -1,40 +1,26 @@
-#ifndef POLYNOMIAL_H
+#ifndef POLINOMIAL_H
 #define POLYNOMIAL_H
 
-class Polynomial {
-  private:
-      int _degree1, _degree2;
-      int *arr1 = new int[_degree1 + 1];
-      int *arr2 = new int[_degree2 + 1];
-      int *arr3;
-  public:
+#include <iostream>
 
-      void Add();
-      void Subtract();
-      void Multiply();
-     Polynomial(int degree1,int degree2,int arrX[],int arrY[]) {
-          _degree1 = degree1;
-          _degree2 = degree2;
-          int n = 0;
-          if(degree1 < degree2) {
-            n = degree2;
-          }
-          else {
-            n = degree1;
-          }
-          arr3 = new int [n];
-
-          for(int i = 0; i <= degree1;i++ ) {
-            *(arr1 + i ) = arrX[i];
-          }
-          for(int i = 0; i <= degree2;i++ ) {
-            *(arr2 + i ) = arrY[i];
-          }
-     } 
-    ~Polynomial() {
-
-    };     
+class Polynomial{
+	private:
+		int * _coefficent;
+		int _arrSize;
+	public:
+		Polynomial();
+		Polynomial(int degree);
+		Polynomial(int *coefficientArr, int coefficientCount);
+		Polynomial(Polynomial& other);
+		Polynomial operator+( const Polynomial &other) const; 
+		Polynomial operator-( const Polynomial &other) const; 
+		Polynomial operator*( const Polynomial &other) const; 
+		Polynomial &operator+=( const Polynomial &other);
+		Polynomial &operator-=( const Polynomial &other);
+		Polynomial &operator*=( const Polynomial &other);
+    std::istream& operator>>(std::istream&, Polynomial&);
+    std::ostream& operator<<(std::ostream&, const Polynomial&);
+		int degree();
+		void print();
 };
-
-
 #endif
