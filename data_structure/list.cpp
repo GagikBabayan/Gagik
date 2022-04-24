3c9c629731a389d8f8dd53251693c561251d7144
GagikBabayan <babayan-gago01@mail.ru>
2022-04-24 22:05:37 +0400

    list.cpp

---
+++ data_structure/list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "linkedlist.h"
+#include "linkedlist.cpp"
+using namespace std;
+
+ 
+int main() 
+{
+    
+   List<int> t;
+   t.Add(4);
+    cout << t;
+
+    return 0;
+}
