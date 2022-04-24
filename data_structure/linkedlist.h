383bfaf45e9d7cd9ef28187532f1b154a07790c4
GagikBabayan <babayan-gago01@mail.ru>
2022-04-24 22:10:17 +0400

    LinkedList.h

---
+++ data_structure/linkedlist.h
@@ -0,0 +1,42 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+template <class T>
+    class Node                                                        // Nodes Class
+    {
+    public:
+        Node* pNext;
+        T data;
+ 
+        Node(T data, Node* pNext = nullptr)
+        {
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+template <class T>
+class List{
+public:
+    List();
+    ~List();
+    void Add(T data);                                                 // Add Nodes
+    void Add_front(T data);                                           // Add Nodes from the beginning
+    void AddAt(T data, int index);                                    // Add Nodes from everything place
+    int GetSize() { return Size; }                                    // Size of Nodes
+    T& operator[](const int index);                                   // Overloading [] operator
+    void Remove_front();                                              //  Remove front Nodes
+    void RemoveAt(int index);                                         //  Remove 
+    void Remove_back();
+    void Clear();
+    friend ostream& operator <<(ostream &out,const List<T> &);
+private:
+    int Size;
+    Node<T>* head;
+};
+
+
+
+
+
+#endif
