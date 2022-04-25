0d154c834192dabb3028fb14a341fb1dc8530b50
GagikBabayan <babayan-gago01@mail.ru>
2022-04-25 21:09:38 +0400

    deque.hpp

---
+++ data_structure/deque/deque.hpp
@@ -0,0 +1,76 @@
+#ifndef __DEQUE_H__
+#define __DEQUE_H__
+
+
+#include "../LinkedList/linkedlist.h"
+
+template<typename T>
+class DeQueue{
+private:
+    List<T> _list;
+
+public:
+    T popFront();
+    void popBack();
+    bool isEmpty();
+    void pushFront(T);
+    void pushBack(T);
+    T peekFront();
+    T peekBack();
+    DeQueue();
+    ~DeQueue();
+};
+
+template<typename T>
+T DeQueue<T>::popFront()
+{ 
+    T return_value = _list[1];
+    _list.Remove_front();
+    return return_value;
+}
+
+template<typename T>
+void DeQueue<T>::popBack()
+{
+    _list.Remove_back(); 
+}
+
+template<typename T>
+bool DeQueue<T>::isEmpty()
+{
+    return _list.GetSize() == 0;
+}
+
+template<typename T>
+void DeQueue<T>::pushFront(T value)
+{
+    _list.Add_front(value);
+}
+template<typename T>
+void DeQueue<T>::pushBack(T value)
+{
+    _list.Add(value);
+}
+
+template<typename T>
+T DeQueue<T>::peekFront()
+{
+    return _list[0];
+}
+
+template<typename T>
+T DeQueue<T>::peekBack()
+{
+    return _list[_list.GetSize() - 1];
+}
+
+template<typename T>
+DeQueue<T>::DeQueue() = default;
+
+template<typename T>
+DeQueue<T>::~DeQueue()
+{
+    _list.Clear();
+}
+
+#endif
