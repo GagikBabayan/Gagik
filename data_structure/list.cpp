20102c6815fef6f161bc7fac51160e5c98f10954
Arman <shahramanyan1995@mail.ru>
2022-04-25 12:09:38 +0400

    List.cpp Modified

--- data_structure/list.cpp
+++ data_structure/list.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "linkedlist.h"
 #include "linkedlist.cpp"
+#include "stack.hpp"
+#include "que.hpp"
 using namespace std;
 
  
@@ -8,8 +10,17 @@ int main()
 {
     
    List<int> t;
-   t.Add(4);
-    cout << t;
+   Stack<int> stack;
+   stack.push(2);
+   stack.push(3);
+   cout << stack.peek() << endl;
+   Queue<int> q;
+   q.pushBack(5);
+   q.pushBack(10);
+
+    cout << q.peek() << endl ;
+   
+    
 
     return 0;
 }
