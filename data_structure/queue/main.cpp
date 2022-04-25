29e45f2043ca22527ddd07b5a8b55d11430555ba
GagikBabayan <babayan-gago01@mail.ru>
2022-04-25 21:10:33 +0400

    Main.cpp

---
+++ data_structure/queue/main.cpp
@@ -0,0 +1,16 @@
+#include "que.hpp"
+#include "../LinkedList/linkedlist.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Queue<int> que;
+    que.pushBack(5);
+    que.pushBack(3);
+    que.popFront();
+    cout << que.peek() << endl;
+
+    return 0;
+}
