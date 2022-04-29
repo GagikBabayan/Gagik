c75de7a8b7341acccff0b27359cd7a72d5795f49
GagikBabayan <100302868+GagikBabayan@users.noreply.github.com>
2022-04-29 09:49:34 +0400

    Update Main.cpp

--- data_structure/Sort/Main.cpp
+++ data_structure/Sort/Main.cpp
@@ -4,6 +4,7 @@
     #include "insertSort.hpp"
     #include "merge.hpp"
     #include "print.hpp"
+    #include "quick.hpp"
 
     mt19937 gen (time(0)); 
 
@@ -18,17 +19,20 @@
     const int size = 15;
     int arr1[size];
     int arr2[size];
-	int arr3[size];
+    int arr3[size];
+    int arr4[size];
 
     for(int i = 0; i < size;i++){
         arr1[i] =  MyGenerator();
         arr2[i] = arr1[i];
-		arr3[i] = arr1[i];
+	arr3[i] = arr1[i];
+        arr4[i] = arr1[i];
     }
 
     int *p1 = arr1;
     int *p2 = arr2;
     int *p3 = arr3;
+    int *p4 = arr4;
     
     // Print(p1,size);
     clock_t start1 = clock();
@@ -61,9 +65,9 @@
 
     // Print(arr3,size);
 
-	p3 = MergeSort(arr3, size);
+    p3 = MergeSort(arr3, size);
     
-	clock_t end3 = clock();
+    clock_t end3 = clock();
     double elapsed3 = double(end3- start3)/CLOCKS_PER_SEC;
 
     cout << "Merge Sorting" << "\n" << "Size = " << size << "\n";
@@ -71,6 +75,19 @@
     cout << endl;
     
     // Print(p3,size);
+
+    clock_t start4 = clock();
+
+    p4 = QuickSort(arr4,0,size - 1);
+    
+    clock_t end4 = clock();
+    double elapsed4 = double(end4- start4)/CLOCKS_PER_SEC;
+
+    cout << "Quick Sorting" << "\n" << "Size = " << size << "\n";
+    cout <<  "Run Time = " << elapsed3;
+    cout << endl;
+    
+     Print(p4,size);
 
     return 0;
 }
