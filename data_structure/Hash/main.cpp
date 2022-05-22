87ec15c2d832d3fd04ea58a7ae6efba9e8eeed04
GagikBabayan <babayan-gago01@mail.ru>
2022-05-22 12:45:48 +0400

    Main function

--- data_structure/Hash/main.cpp
+++ data_structure/Hash/main.cpp
@@ -1,35 +1,21 @@
 #include "hash.hpp"
 
-int main()
-{
+int main() {
+  HashTable a;
+  a.insert("Gagik", 21);
+  a.insert("Abo", 27);
+  a.insert("Alexandr", 24);
+  a.insert("Vahe", 25);
+  a.remove("Vahe");
 
-    int a[] = {15, 11, 27, 8, 12};
-    int n = sizeof(a) / sizeof(a[0]);
+  a.resize_X2();
+  a.print();
 
-    string b[] = {"hello","world","Armenia"};
-    int k = sizeof(b)/sizeof(b[0]);
-
+  cout << "countElement = " << a.countElement() << endl;
+  cout << a.find("Alexandr") << endl;
+  cout << endl;
   
-    Hash<int> h(7);
-    for (int i = 0; i < n; i++)
-    {
-      h.insertItem(a[i]);
-    }
-    h.displayHash();
-    cout << "/////////////////////" << endl;
-    h.deleteItem(12);
-    h.displayHash();
-  
-    /*
-    Hash<string> h(6);
-    for(int i = 0; i < k; k++)
-    {
-      h.insertItem(b[i]);
-    }
-     h.displayHash();
-    cout << "/////////////////////" << endl;
-    h.deleteItem(b[0]);
-    h.displayHash();
-    */
+ 
+
   return 0;
 }
