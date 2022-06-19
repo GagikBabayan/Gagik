b3fc32ef2210cc3dd5cebc6bc6be22a8333969fa
GagikBabayan <100302868+GagikBabayan@users.noreply.github.com>
2022-06-19 20:05:35 +0400

    Update script2.js

--- Javascript/homework3/script2.js
+++ Javascript/homework3/script2.js
@@ -2,12 +2,15 @@ function add(number1, number2) {
   if (number2 != undefined) {
     return number1 + number2;
   }
-  return function generic(number2) {
-    if (typeof number2 == "number") {
-      return number1 + number2;
-    }
-    return generic;
-  };
+  if (typeof number1 == "number") {
+    return function generic(number2) {
+      if (typeof number2 == "number") {
+        return number1 + number2;
+      }
+      return generic;
+    };
+  }
+  return add;
 }
 
 console.log(add(7, 15)); // -> 22
@@ -15,7 +18,7 @@ console.log(add(7)(15)); // -> 22
 console.log(add(7)()(15)); // -> 22
 console.log(add(7)()()()(15)); // -> 22
 console.log(add(7)()()()()()()()()()()()(15)); // -> 22
-// console.log(add()(7)(15)); // -> 22
-// console.log(add()()()()(7)(15)); // -> 22
-// console.log(add()(7)()(15)); // -> 22
-// console.log(add()()()()()(7)()()()(15)); // -> 22
+console.log(add()(7)(15)); // -> 22
+console.log(add()()()()(7)(15)); // -> 22
+console.log(add()(7)()(15)); // -> 22
+console.log(add()()()()()(7)()()()(15)); // -> 22
