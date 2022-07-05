18f9897add9a9992616a9b40ea6549166932a889
GagikBabayan <100302868+GagikBabayan@users.noreply.github.com>
2022-07-05 09:37:32 +0400

    Add files via upload

---
+++ Javascript/moviesModification/src/Homepage.js
@@ -0,0 +1,12 @@
+import menu from "./images/menu.jpeg";
+import "./Menu/Menu.css";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 id="Welcome">Welcome to the World of Films!</h1>
+    </div>
+  );
+};
+
+export { Homepage };
