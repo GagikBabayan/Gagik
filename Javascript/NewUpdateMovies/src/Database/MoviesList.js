b5b8d1ef9abda4559a49723a61b45bb55a951b75
GagikBabayan <100302868+GagikBabayan@users.noreply.github.com>
2022-07-07 00:59:35 +0400

    Create MoviesList.js

---
+++ Javascript/NewUpdateMovies/src/Database/MoviesList.js
@@ -0,0 +1,90 @@
+import Spider from "../images/download.jpeg";
+import Eternals from "../images/Eternals.jpeg";
+import Splite from "../images/splite.jpeg";
+import Venom from "../images/venom.jpeg";
+import blackWidow from "../images/blackWidow.jpeg";
+import Thor from "../images/thor.jpeg";
+import docterStrange from "../images/docterStrange.jpeg";
+import Vanguard from "../images/Vanguard.jpeg";
+import IronMan3 from "../images/Iron_Man_3.jpeg";
+import Jurassic from "../images/Jurassic_World.jpeg";
+import Divergent from "../images/Divergent.jpeg";
+import Pirates from "../images/Pirates.jpeg";
+
+const moviesList = [
+  {
+    id: 0,
+    name: "Spider-Man",
+    image: Spider,
+    text: "Peter Parker, an outcast high school student, gets bitten by a radioactive spider and attains superpowers. Soon, he is forced to use his abilities to fight a monstrous foe.",
+  },
+  {
+    id: 1,
+    name: "Eternals",
+    image: Eternals,
+    text: " The Eternals, a race of immortal beings with superhuman powers who have secretly lived on Earth for thousands of years, reunite to battle the evil Deviants.",
+  },
+  {
+    id: 2,
+    name: "Splite",
+    image: Splite,
+    text: "Kevin, who is suffering from dissociative identity disorder and has 23 alter egos, kidnaps three teenagers. They must figure out his friendly personas before he unleashes his 24th personality.",
+  },
+
+  {
+    id: 3,
+    name: "Venom",
+    image: Venom,
+    text: "While trying to take down Carlton, the CEO of Life Foundation, Eddie, a journalist, investigates experiments of human trials. Unwittingly, he gets merged with a symbiotic alien with lethal abilities.",
+  },
+  {
+    id: 4,
+    name: "Black Widow",
+    image: blackWidow,
+    text: "Natasha Romanoff, a former KGB spy, is shocked to find out that her ex handler, General Dreykov, is still alive. While evading capture by Taskmaster, she is forced to confront her dark past.",
+  },
+  {
+    id: 5,
+    name: "Thor",
+    image: Thor,
+    text: "Thor embarks on a journey unlike anything he's ever faced -- a quest for inner peace. However, his retirement gets interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods.",
+  },
+  {
+    id: 6,
+    name: "Doctor Strange",
+    image: docterStrange,
+    text: "A terrible car accident put an end to the career of a successful neurosurgeon Doctor Strange. Desperate, he embarks on a journey in search of healing and discovers his incredible ability to transform space and time.",
+  },
+  {
+    id: 7,
+    name: "Vanguard: Arctic Wolves",
+    image: Vanguard,
+    text: "They take on jobs that others can't. When an international mercenary syndicate kidnaps a well-known businessman, the only chance to save him is the dashing Vanguard team, which has no equal in skill and audacity.",
+  },
+  {
+    id: 8,
+    name: "Iron Man 3",
+    image: IronMan3,
+    text: "As Stark's world crumbles before his eyes through the fault of unknown adversaries, Tony yearns to find the culprits and settle scores with them. Caught in a hopeless situation, Stark can only rely on himself and his ingenuity to protect those he cares about.",
+  },
+  {
+    id: 9,
+    name: "Jurassic World",
+    image: Jurassic,
+    text: "Thousands of people rush to see the Jurassic World, but seemingly safe entertainment can turn into a threat to all mankind...",
+  },
+  {
+    id: 10,
+    name: "Divergent",
+    image: Divergent,
+    text: "The action develops on Earth in the future, in despotic Chicago. All teenagers who have reached the age of 16 are required to choose one of the five factions and join it for the rest of their lives. Each faction represents a certain quality: sincerity, renunciation, fearlessness, friendliness and erudition.",
+  },
+  {
+    id: 11,
+    name: "Pirates of the Caribbean",
+    image: Pirates,
+    text: "Charismatic adventurer Captain Jack Sparrow's adventure-filled life takes a dramatic turn when his nemesis, Captain Barbossa, steals Jack's ship, the Black Pearl, then attacks Port Royal and steals the governor's beautiful daughter, Elizabeth Swann.",
+  },
+];
+
+export { moviesList };
